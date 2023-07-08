ed16999360736b09c13a6fd1269b1a4790521078
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-07-08 11:56:12 -0400

    Agregando el detalle.

--- Server/Controllers/IngresosController.cs
+++ Server/Controllers/IngresosController.cs
@@ -27,6 +27,16 @@ public class IngresosController : ControllerBase
         return await _Context.Ingresos.ToListAsync();
     }
 
+    /*[HttpGet]
+     public async Task<ActionResult<IEnumerable<Practi>>> GetPracti()
+    {
+        if(_context.Preacti == null){
+            RETURN NoFoud();
+        }
+        return await _contect.Practi.ToListAsync();
+    }
+     */
+
     [HttpGet("{id}")]
     public async Task<ActionResult<Ingresos>> GetIngresos(int id)
     {
@@ -43,6 +53,23 @@ public class IngresosController : ControllerBase
         }
         return ingresos;
     }
+    /*
+     [HttpGet("{id}")]
+    public async Task<ActionResult<Practi>> GetPracti(id)
+    {
+        if(_context.Practi == nuul)
+        {
+            return NoFound();
+        }
+        
+        var practi = await _context.Practi.FindAsync(id);
+        if(practi == null){
+            retutn nofounf();
+        }
+    return practi;
+     
+    }
+     */
 
     [HttpPost]
     public async Task<ActionResult<Ingresos>> PostIngresos(Ingresos ingresos)
@@ -55,6 +82,19 @@ public class IngresosController : ControllerBase
         await _Context.SaveChangesAsync();
         return Ok(ingresos);
     }
+
+    /*
+     [HttpPost]
+    public async Task<ActionResult<Practi>> PostPracti(Practica practi){
+        if(!Existe(practi.practiid))
+            _context.Practica.Add(practi);
+        else
+            _context.Practica.Update(practi);
+
+        await _context.SaveChangeAsync();
+        return Ok(practi);
+    }
+     */
 
     [HttpDelete]
     public async Task<ActionResult> DeleteIngresos(int id)
@@ -76,11 +116,36 @@ public class IngresosController : ControllerBase
 
         return NoContent();
     }
+    /*
+     [HttpDelete]
+    public async Task<ActionResult> DeletePractica(int id)
+    {
+        if(_context.Practica == null)
+        {
+            return NoFound();
+        }
+        var precti = await _contexto.Practica.FindAsync(id);
+
+        if(precti == null)
+    {
+           return NoFound();
+    }
+        _context.Practica.Remove(precti);
+        await _context.AddRangeAsync(precti);
+        return NoContent();
+    }
+     */
 
     private bool Existe(int id)
     {
         return (_Context.Ingresos?.Any(i => i.IngresoId == id)).GetValueOrDefault();
     }
+    /*
+        private bool Existe(int id)
+    {
+        return (_contexto.Prectica?.Any(p => p.PractiID == id)).GetValueOrdefault();
+    }
+     */
 }
 
 
---
+++ Shared/Models/IngresosDetalle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IngresosWabA.Shared.Models
+{
+	public class IngresosDetalle
+	{
+		[Key]
+
+		public string? Emisor { get; set; }
+
+        public string? Mensaje { get; set; }
+    }
+}
