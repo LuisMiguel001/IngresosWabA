b51031817b6e1b84d3705b4c657ee463534ff552
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-07-07 20:37:35 -0400

    Agregando el modelo.

---
+++ Shared/Models/Ingresos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IngresosWabA.Shared.Models
+{
+    public class Ingresos
+    {
+        [Key]
+
+        [Required (ErrorMessage = "IngresosId")]
+        public int IngresoId { get; set; }
+
+        [Required(ErrorMessage = "Fecha")]
+        public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "Concepto")]
+        public string? Concepto { get; set; }
+
+        [Required(ErrorMessage = "Monto")]
+        public decimal Monto { get; set; }
+    }
+}
